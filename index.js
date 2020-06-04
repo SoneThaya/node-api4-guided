730f9b379dfc6c8be7b641bb8651402cadca1171
Sone <sthaya@gmail.com>
2020-06-04 09:28:50 -0700

    added start script

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+// heroku will add the PORT variable to the environment
+
+const process = {
+  env: { // the OS/computer where the app is executing
+    PORT: 
+  }
+}
+
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });

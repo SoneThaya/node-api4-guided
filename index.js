375c1a5714885d3183e29f1a29c548cd25695fca
Sone <sthaya@gmail.com>
2020-06-04 09:33:27 -0700

    remoted a file

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const server = require("./api/server.js");
 
 // heroku will add the PORT variable to the environment
 
-const process = {
-  env: { // the OS/computer where the app is executing
-    PORT: 
-  }
-}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
